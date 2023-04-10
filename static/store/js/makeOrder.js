2e1caf33054e397727d3748646c6f12b5924b814
Shmarev Dmitry <shds-2111@yandex.ru>
2023-04-11 02:02:01 +0300

    очередной коммит

--- static/store/js/makeOrder.js
+++ static/store/js/makeOrder.js
@@ -12,14 +12,15 @@ makeOrderBtn[0].addEventListener('click', function (){
 function makeOrder(){
     console.log('Making order...')
     let url = '/make_order/'
-
+    let f = document.getElementById('address')
+    const address = f.value;
     fetch(url, {
         method: 'POST',
         headers:{
             'Content-Type':'application/json',
             'X-CSRFToken':csrftoken,
         },
-        body:JSON.stringify({})
+        body:JSON.stringify({'address': address})
     })
         .then((response)=>{
             return response.json()
--- store/views.py
+++ store/views.py
@@ -2,11 +2,12 @@ import json
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.views import View
 from django.views.generic import ListView
 
-from .models import Product, Order, OrderItem, DeliveryDate
+from .models import Product, Order, OrderItem, DeliveryDate, DeliveryAddress
 
 
 class ProductListView(ListView):
@@ -27,26 +28,34 @@ class CartCountView(View):
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
         user = data['user']
-        items = Order.objects.get(customer__username=user,
-                                  status='not formed').get_total_items
+        if Order.objects.filter(customer__username=user,
+                                status='not formed').exists():
+            items = Order.objects.get(customer__username=user,
+                                      status='not formed').get_total_items
+        else:
+            items = 0
         return JsonResponse({'count': items})
 
 
 def basket(request):
-    if request.user.is_authenticated:
-        order, created = Order.objects.get_or_create(
-            customer=request.user,
-            status='not formed')
+    addresses = DeliveryAddress.objects.all()
+    if request.user.is_authenticated and Order.objects.filter(
+            customer=request.user, status='not formed').exists():
+        order = Order.objects.get(customer=request.user,
+                                  status='not formed')
         if DeliveryDate.objects.filter(status='open').exists():
             order.delivery_date = DeliveryDate.objects.get(status='open')
             order.save()
         items = order.orderitem_set.all()
+        addresses = DeliveryAddress.objects.all()
     else:
         items = []
         order = {'get_total_items': 0,
-                 'get_total_price': 0}
+                 'get_total_price': 0,
+                 'addresses': addresses}
     return render(request, 'store/basket.html',
-                  context={'items': items, 'order': order})
+                  context={'items': items, 'order': order,
+                           'addresses': addresses})
 
 
 @login_required()
@@ -78,17 +87,28 @@ def update_item(request):
         order_item.save()
         if order_item.quantity <= 0:
             order_item.delete()
+        if order.get_total_items == 0:
+            order.delete()
+            return JsonResponse({"quantity": 0, "total_items": 0}, status=200)
         return JsonResponse({"quantity": order_item.quantity,
                              "total_items": order.get_total_items}, status=200)
     return JsonResponse('GET method not allowed', safe=False)
 
 
 def make_order(request):
-    if Order.objects.filter(customer=request.user,
-                            status='not formed').exists():
-        order = Order.objects.get(customer=request.user,
-                                  status='not formed')
-        order.status = 'formed'
-        order.save()
-        return JsonResponse('Order formed')
-    return JsonResponse('No products in basket. Order not formed.')
+    if request.method == 'POST':
+        if Order.objects.filter(customer=request.user,
+                                status='not formed').exists():
+            data = json.loads(request.body)
+            address_id = data['address']
+            address = DeliveryAddress.objects.get(id=address_id)
+            order = Order.objects.get(customer=request.user,
+                                      status='not formed')
+            order.status = 'formed'
+            order.address = address
+            order.date = timezone.now()
+            order.save()
+            return redirect('orders')
+        return JsonResponse('No products in basket. Order not formed.',
+                            safe=False)
+    return JsonResponse('GET method not allowed', safe=False)
